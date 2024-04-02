472936be6f6913a170fd69c4a7e03f05e1f9de1a
Brody Cooper <reed.thorngag@gmail.com>
2024-04-03 10:25:08 +1300

    still refactoring

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # SCP Polytech Project
-A full stack project for polytech which uses express, postgres and vanilla js
+A full stack project for polytech which uses express, postgres and react
+
+The backend is stored in backend/ the frontend is stored in react-app/, which has its own package.json so it can be run independantly in a seperate docker container for development which is what the DockerFile is for, otherwise for production it is built once when the docker container is created, this means you can have auto reload on both the backend and frontend simultaniously. for development the react app runs on port 3000
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,31 +1,40 @@
-import './preinitilization';
-import './tests/run_tests';
-import app from './server';
-import logger from './util/logger';
+import './preinitilization.js';
+import app from './server.js';
+import Logger from './util/logger';
 import router from './router';
 import mock_data from './mock_data';
 import https from 'https';
 import http from 'http';
 import fs from 'fs';
+import express from 'express';
+import local_router from './local_routes.js';
 
 mock_data();
 
 // NOTE: prismaClient and authenticator are global variables, be careful not to overwrite them (declared in preinitilization.ts)
 
-const process_port:string = process.env.PORT||"0";
-const port: number = parseInt(process_port) || 443;
+const port: number = 443;
 
 app.use('/api',router);
-
-app.post('/test',(req,res)=>{
-});
+const local_app = express().use('/',local_router);
 
 var privateKey = fs.readFileSync( 'privatekey.key' );
 var certificate = fs.readFileSync( 'certificate.crt' );
 
+if (process.env.ENV === 'DEV')
+    http.createServer(app).listen(port,()=>{
+        Logger.info("listening on port "+port);
+        http.createServer(local_app).listen(80);
+    });
+else {
+    var privateKey = fs.readFileSync('./privatekey.key' );
+    var certificate = fs.readFileSync('./certificate.crt' );  
+    https.createServer({
+        key: privateKey,
+        cert: certificate
+    }, app).listen(port,()=>{
+        http.createServer(local_app).listen(80);
+        Logger.info(`listening on port ${port}`);
 
-http.createServer(app).listen(80);
-https.createServer({
-    key: privateKey,
-    cert: certificate
-}, app).listen(port,()=>{logger.info(`listening on port ${port}`)});
+    });
+}
